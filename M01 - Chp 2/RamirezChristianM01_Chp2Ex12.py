d2bfe42dfc3cde3dedbee727bbb607e46fd7647b
Christian <qrisstian98@gmail.com>
2026-08-26 18:24:49 -0400

    final draft

--- M01 - Chp 2/RamirezChristianM01_Chp2Ex12.py
+++ M01 - Chp 2/RamirezChristianM01_Chp2Ex12.py
@@ -8,7 +8,7 @@ stockbroker_fee_amount = share_cost*stockbroker_fee
 print("This is the initial cost of the shares:$",f"{share_cost:.2f}")
 print("This is the stockbroker fee:$",f"{stockbroker_fee_amount:.2f}")
 
-"""this is now the sold/second portion"""
+#this is now the sold/second portion
 
 total_shares_sold = 2000
 share_price = 42.75
