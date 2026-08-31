861501c6e451de03be77c57df119bb097f723c02
Christian <qrisstian98@gmail.com>
2026-08-31 19:04:07 -0400

    draft

---
+++ M02 - Chp 3/RamirezFloresChristianM02_Chp3_Ex12.py
@@ -0,0 +1,11 @@
+"""
+For this and all assignments, you are expected to follow good programming standards.
+
+Please name the .py and .docx files for each exercise using the name LNameFnameM#_Ch#Ex# (using your name). 
+For example: GarvinFredM02_Ch2Ex14.py and GarvinFredM02_Ch2Ex14.docx (Python and Word will automatically append the extensions).
+
+Assignment:
+Complete Programming Exercises 12, 16 and 17 at the end of Chapter 3.  
+For each exercise, submit the .py file and the single Word doc containing screen shots showing your name in the output.  
+(6 files total to submit)
+"""
