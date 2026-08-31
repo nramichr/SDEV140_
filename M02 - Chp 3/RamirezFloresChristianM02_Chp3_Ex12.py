01b09625c91e7151c399a298bd96d9ba590a4673
Christian <qrisstian98@gmail.com>
2026-08-31 19:34:07 -0400

    Final Draft

--- M02 - Chp 3/RamirezFloresChristianM02_Chp3_Ex12.py
+++ M02 - Chp 3/RamirezFloresChristianM02_Chp3_Ex12.py
@@ -1,11 +1,38 @@
 """
-For this and all assignments, you are expected to follow good programming standards.
+A software company sells a package that retails for $99. Quantity discounts are given according to the following 
++-------------+---------+
+|Quantity     |Discount |
++-------------+---------+
+| 10~19       | 10%     |
+| 20~49       | 20%     |
+| 50~99       | 30%     |
+| 100 or more | 40%     |
++-------------+---------+
 
-Please name the .py and .docx files for each exercise using the name LNameFnameM#_Ch#Ex# (using your name). 
-For example: GarvinFredM02_Ch2Ex14.py and GarvinFredM02_Ch2Ex14.docx (Python and Word will automatically append the extensions).
-
-Assignment:
-Complete Programming Exercises 12, 16 and 17 at the end of Chapter 3.  
-For each exercise, submit the .py file and the single Word doc containing screen shots showing your name in the output.  
-(6 files total to submit)
+Write a program that asks the user to enter the number of packages purchased. 
+The program should then display the amount of the discount (if any) and the total amount of the purchase after the discount.
 """
+
+def main():
+    packages_purchased: int = int(input("Enter number of packages purchased: "))
+
+    if packages_purchased >= 100:
+        discount = 0.4
+    elif packages_purchased >= 50 and packages_purchased <= 99:
+        discount = 0.3
+    elif packages_purchased >= 20 and packages_purchased <= 49:
+        discount = 0.2
+    elif packages_purchased >= 10 and packages_purchased <= 19:
+        discount = 0.1
+    else:
+        discount = 0
+
+    total_cost = packages_purchased * 99
+    discount_amount = total_cost * discount
+    final_cost = total_cost - discount_amount
+
+    print(f"Discount: ${discount_amount:,.2f}")
+    print(f"Total cost after discount: ${final_cost:,.2f}")
+main()
+
+print('Christian Ramirez Flores')
