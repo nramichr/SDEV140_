9a6fe89cf7436302878bb2a1424eea1b8b3f0aef
Christian <qrisstian98@gmail.com>
2026-09-17 11:23:22 -0400

    Final Draft

--- M04 - Chp 7/Ramirez-Flores,Christian_M04_Ch7Ex8.py
+++ M04 - Chp 7/Ramirez-Flores,Christian_M04_Ch7Ex8.py
@@ -9,20 +9,26 @@ will display a message indicating whether the name was among the most popular.
 (You can access the Computer Science Portal at www.pearsonhighered.com/gaddis.)
 
 """
+#For this program to function your terminal directory must be set to 
+#folder containing this program and the two files.
+
 
 def main():
-    with open ('BoyNames.txt', 'r') as file:
+    with open('BoyNames.txt', 'r') as file:
         boy_names = [line.strip() for line in file]
-    with open ('GirlNames.txt', 'r') as file:
+    with open('GirlNames.txt', 'r') as file:
         girl_names = [line.strip() for line in file]
 
     name = input('Enter a name:')
 
     if name in boy_names and name in girl_names:
         print(f'{name} was one of the 400 most popular names, for both boys and girls.')
-    elif name in boy_names
+    elif name in boy_names:
         print(f'{name} was one of the 400 most popular boy names.')
-    elif name in girl_names
+    elif name in girl_names:
         print(f'{name} was on of the 400 most popular girl names.')
     else:
         print(f"{name} wasn't among the most popular names.")
+main()
+
+print("Christian Ramirez-Flores")
